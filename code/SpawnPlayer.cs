d955c40367fcc4686e09958aeb5bf03c4e8e3daf
Nolankicks <nolandimensions@gmail.com>
2024-01-08 17:30:07 -0500

    Update SpawnPlayer.cs

--- code/SpawnPlayer.cs
+++ code/SpawnPlayer.cs
@@ -2,8 +2,27 @@ using Sandbox;
 
 public sealed class SpawnPlayer : Component
 {
+
+
+	[Property] public GameObject objectToSpawn {get; set;}
+	
 	protected override void OnUpdate()
 	{
+		var pc = Components.Get<PlayerController>();
+		var lookDir = pc.EyeAngles.ToRotation();
+		
+		
+		if (Input.Pressed("Attack1"))
+		{
+			var pos = Transform.Position + Vector3.Up * 200.0f + lookDir.Forward.WithZ( 0.0f ) * 200.0f;
+			SceneUtility.Instantiate( objectToSpawn, pos );
+	
+			
+			
+
+
+		}
 
 	}
+
 }
