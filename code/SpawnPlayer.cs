3bfa92999032b219c33380ab956fe87dbe4dd8ee
Nolankicks <nolandimensions@gmail.com>
2024-01-08 19:12:52 -0500

    More fixes

--- code/SpawnPlayer.cs
+++ code/SpawnPlayer.cs
@@ -1,10 +1,18 @@
 using Sandbox;
+using Sandbox.Razor;
 
 public sealed class SpawnPlayer : Component
 {
 
 
-	[Property] public GameObject objectToSpawn {get; set;}
+	[Property] public GameObject Attack1 {get; set;}
+	[Property] public GameObject Attack2 {get; set;}
+	[Property] public SoundEvent soundEvent {get; set;}
+	[Property] Rotation rotation {get; set;}
+	[Property] SceneFile reloadScene {get; set;}
+
+
+
 	
 	protected override void OnUpdate()
 	{
@@ -15,14 +23,33 @@ public sealed class SpawnPlayer : Component
 		if (Input.Pressed("Attack1"))
 		{
 			var pos = Transform.Position + Vector3.Up * 200.0f + lookDir.Forward.WithZ( 0.0f ) * 200.0f;
-			SceneUtility.Instantiate( objectToSpawn, pos );
-	
+			SceneUtility.Instantiate( Attack1, pos );
+			Sound.Play(soundEvent);
 			
 			
 
 
 		}
 
+		if (Input.Pressed("Attack2"))
+		{
+		var pos = Transform.Position + Vector3.Up * 1.0f + lookDir.Forward.WithZ( 0.0f ) * 200.0f;
+			SceneUtility.Instantiate( Attack2, pos, rotation );
+			Sound.Play(soundEvent);
+	
+
+
+
+			
+		}
+
+
+
+		if (Input.Pressed("reload"))
+		{
+			GameManager.ActiveScene.Load(reloadScene);
+		}
 	}
+
 
 }
