952d88840d33f0f82141288b92ff465f37a3d651
Nolankicks <nolandimensions@gmail.com>
2024-01-08 21:32:36 -0500

    Commit

---
+++ code/PlayerSpawnLevel4.cs
@@ -0,0 +1,41 @@
+using Sandbox;
+using Sandbox.Razor;
+
+public sealed class PlayerSpawnLevel4 : Component
+{
+
+
+	[Property] public GameObject Attack1 {get; set;}
+	[Property] public SoundEvent soundEvent {get; set;}
+
+
+
+
+	
+	protected override void OnUpdate()
+	{
+		var pc = Components.Get<PlayerController>();
+		var lookDir = pc.EyeAngles.ToRotation();
+		
+		
+		if (Input.Pressed("Attack1"))
+		{
+			var pos = Transform.Position + Vector3.Up * 200.0f + lookDir.Forward.WithZ( 0.0f ) * 200.0f;
+			SceneUtility.Instantiate( Attack1, pos );
+			Sound.Play(soundEvent);
+			
+			
+
+
+		}
+
+
+
+
+
+
+
+
+}
+
+}
