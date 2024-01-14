d60f31459b3cc607ee7684b4dfdb39516d772303
Nolankicks <nolandimensions@gmail.com>
2024-01-14 14:00:12 -0500

    Commit

--- code/Shooter.cs
+++ code/Shooter.cs
@@ -4,21 +4,25 @@ public sealed class Shooter : Component
 {
 	[Property] GameObject bullet {get; set;}
 	[Property] SoundEvent soundEvent {get; set;}
+
+
 	protected override void OnFixedUpdate()
 	{
-		if ( IsProxy )
+				if ( IsProxy )
 			return;
 		var pc = Components.Get<PlayerController2>();
 		var lookDir = pc.EyeAngles.ToRotation();
 		if ( Input.Pressed( "Attack1" ) )
 		{
 			var pos = Transform.Position + Vector3.Up * 64.0f;
+
 			var o = bullet.Clone( pos);
+
+
 			var p = o.Components.Get<Rigidbody>();
 			p.Velocity = lookDir.Forward * 5000f;
 			soundEvent.UI = true;
 			Sound.Play(soundEvent);
-			o.Network.Spawn();
 
 
 		}
